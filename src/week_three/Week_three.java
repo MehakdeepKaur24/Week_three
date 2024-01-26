4b1c9dbd99d5185f3ff6d13c29bfcd171dc7ee69
MehakdeepKaur24 <157150187+MehakdeepKaur24@users.noreply.github.com>
2024-01-26 12:39:24 -0500

    Update Week_three.java

--- src/week_three/Week_three.java
+++ src/week_three/Week_three.java
@@ -7,6 +7,7 @@ package week_three;
 /**
  *
  * @author Mehakdeep Kaur
+ @Modifiedby Mehak on 26th Jan,2024
  */
 public class Week_three {
 
@@ -16,6 +17,7 @@ public class Week_three {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Yes the change is successful");
     }
     
 }
